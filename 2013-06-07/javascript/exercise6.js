d3e0a9d96ee04e1194479439aae18a1270c8594e
m4v3r1ck <federico.mione@gmail.com>
2013-06-08 20:52:36 +0200

    HomeWork3

--- 2013-06-07/javascript/exercise6.js
+++ 2013-06-07/javascript/exercise6.js
@@ -22,10 +22,26 @@ V= [[8,6],
 	[13,6],
 	[3,5]];
 
-
-
 /* FV: matrice compatta delle facce 2D del modello  */
+var FV = new Array()
 
-var lar_to_obj = function(v,fv){  	
+FV = [[5,6,7,8],
+	[0,5,8],
+[0,4,5],
+[1,2,4,5],
+[2,3,5,6],
+[0,8,7],[3,6,7],[1,2,3],[0,1,4]]
+
+var lar_to_obj = function(v,fv){  
+	var p=" Vertici V: ";
+	p = p+"\n";
+
+	for(var i=0; i<V.length, i++)
+	{
+		p=p+"v1: x="+V[i][0]+" y="+V[i][1]+"\n";
+	}
+
+	return p;
+
 }
 
